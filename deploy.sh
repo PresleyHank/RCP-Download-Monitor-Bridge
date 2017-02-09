746c43419fe32abf7a1ee1a0196b305d07085949
John Parris <public@johnparris.com>
2017-02-09 16:22:21 -0500

    Update deploy script PLUGINSLUG var to match wporg

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 # The difference is that this script lives in the plugin's git repo & doesn't require an existing SVN repo.
 
 # main config
-PLUGINSLUG=${PWD##*/} # returns basename of current directory
+PLUGINSLUG="restrict-content-pro-download-monitor" # returns basename of current directory
 CURRENTDIR=`pwd`
 MAINFILE="rcp-download-monitor.php" # this should be the name of your main php file in the wordpress plugin
 
